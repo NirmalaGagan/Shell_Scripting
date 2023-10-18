9445fa40ba0bbca562b062a0d564975325983370
NirmalaGagan <genekehalnirmala1@gmail.com>
2023-10-18 11:00:36 +0530

    updated

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -6,7 +6,7 @@ source components/common.sh    # Source loads a file and this file has all the c
 echo -e "\e[32m ______ $COMPONENT Configuration is Starting  _________ \e[0m"
 
 echo -n "Installing Nginx :"
-yum install nginx -y     &>> "${LOFGILE}"
+yum install nginx -y &>> "${LOFGILE}"
 stat $? 
 
 echo -n "Downloading the $COMPONENT :"
@@ -15,29 +15,29 @@ stat $?
 
 echo -n "Clearing the default content : "
 cd /usr/share/nginx/html
-rm -rf *    &>> "${LOFGILE}"
+rm -rf * &>> "${LOFGILE}"
 stat $? 
 
 echo -n "Extracting $COMPONENT : "
-unzip -o /tmp/$COMPONENT.zip  &>> "${LOFGILE}"
+unzip -o /tmp/$COMPONENT.zip &>> "${LOFGILE}"
 stat $? 
 
 echo -n "Copying $COMPONENT :"
-mv frontend-main/* .   &>> "${LOFGILE}"
-mv static/* .          &>> "${LOFGILE}"
-rm -rf frontend-main README.md  &>> /tmp/frontend.log 
-mv localhost.conf /etc/nginx/default.d/roboshop.conf   &>> "${LOFGILE}"
+mv frontend-main/* . &>> "${LOFGILE}"
+mv static/* . &>> "${LOFGILE}"
+rm -rf frontend-main README.md &>> /tmp/frontend.log 
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>> "${LOFGILE}"
 stat $? 
 
 for component in catalogue cart user shipping payment; do     # for loop 
     echo -n "Updating the $component backend reverse proxy dns records:"
-    sed -i -e "/$component/s/localhost/$component.roboshop.internal/"  /etc/nginx/default.d/roboshop.conf  
+    sed -i -e "/$component/s/localhost/$component.roboshop.internal/" /etc/nginx/default.d/roboshop.conf  
     stat $? 
 done 
  
 echo -n "Restarting Nignx :"
-systemctl enable nginx    &>> "${LOFGILE}"
-systemctl restart nginx   &>> "${LOFGILE}"
+systemctl enable nginx &>> "${LOFGILE}"
+systemctl restart nginx &>> "${LOFGILE}"
 stat $?
 
 echo -e "\e[32m ______ $COMPONENT Configuration Completed _________ \e[0m"
